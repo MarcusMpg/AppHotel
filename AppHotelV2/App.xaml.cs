023b5559fd7a6db1bf32033507b0b60eba76fd12
Marcus Mathues <107503566+marcusmpg@users.noreply.github.com>
2024-06-08 01:23:04 -0300

    new functions add

--- AppHotelV2/App.xaml.cs
+++ AppHotelV2/App.xaml.cs
@@ -1,7 +1,36 @@
-﻿namespace AppHotelV2
+﻿using AppHotelV2.Models;
+
+namespace AppHotelV2
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 80.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 89.90,
+                ValorDiariaCrianca = 60.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 60.0,
+                ValorDiariaCrianca = 40.0
+            }, new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25.0,
+                ValorDiariaCrianca = 12.0
+            }
+        };
         public App()
         {
             InitializeComponent();
@@ -13,7 +42,7 @@
             var window = base.CreateWindow(activatioState);
 
             window.Width = 600;
-            window.Height = 400;
+            window.Height = 700;
 
             return window;
         }
---
+++ AppHotelV2/Models/Hospedagem.cs
@@ -0,0 +1,30 @@
+﻿namespace AppHotelV2.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+        public int QntAdultos { get; set; }
+        public int QntCriancas { get; set; }
+
+        public DateTime DataCheckin { get; set; }
+        public DateTime DataCheckout { get; set; }
+
+        public int Estadia
+        {
+            get => DataCheckout.Subtract(DataCheckin).Days;
+        }
+
+        public double ValorTotal
+        {
+            get
+            {
+                double Valor_adultos = QntAdultos*QuartoSelecionado.ValorDiariaAdulto;
+                double Valor_criancas = QntCriancas*QuartoSelecionado.ValorDiariaCrianca;
+
+                double total = (Valor_adultos+Valor_criancas) * Estadia;
+
+                return total;
+            }
+        }
+    }
+}
